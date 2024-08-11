2a47e447f3a74af33b3d8c61553f38cfaaf502ea
SuzanFah <susiefahim@gmail.com>
2024-08-12 00:27:39 +0300

    fixing pip scripts errors

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,20 +1,60 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application HBNB"""
-from flask import Flask
+"""
+Starts a Flask web application with Flasgger for API documentation
+"""
+
+from flask import Flask, render_template
+from flasgger import Swagger
+from models import storage
+
 app = Flask(__name__)
+swagger = Swagger(app)
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    Displays a HTML page like 6-index.html from static
+    ---
+    tags:
+      - Filters
+    parameters:
+      - name: state_id
+        in: query
+        type: string
+        description: The ID of the state to filter by
+    responses:
+      200:
+        description: A page with filters
+        schema:
+          id: Filters
+          properties:
+            states:
+              type: array
+              items:
+                type: object
+                properties:
+                  id:
+                    type: string
+                  name:
+                    type: string
+            amenities:
+              type: array
+              items:
+                type: object
+                properties:
+                  id:
+                    type: string
+                  name:
+                    type: string
+    """
+    states = storage.all('State').values()
+    amenities = storage.all('Amenity').values()
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Prints a Message when /hbnb is called """
-    return 'HBNB'
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Add your SQLAlchemy configuration here
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hbnb_dev:your_password@localhost/your_database_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Import your routes here
+from web_flask import routes
