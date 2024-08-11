cf64960cc5cb4c7fc55a06d74e78f45079d32dd4
SuzanFah <susiefahim@gmail.com>
2024-08-11 23:46:29 +0300

    commiting pip version

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,17 @@
-storage_t = "db"  # or "file", depending on your configuration
+#!/usr/bin/python3
+"""
+Initializes the models package
+"""
+
+from os import getenv
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+storage.reload()
--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""Flask app to generate html page containing popdown menu of states/cities"""
-from flask import Flask, render_template
-from models import storage
-app = Flask('web_flask')
-app.url_map.strict_slashes = False
+""" Starts a Flash Web Application HBNB"""
+from flask import Flask
+app = Flask(__name__)
 
 
-@app.route('/hbnb_filters')
-def display_filters():
-    """Generate page with popdown menu of states/cities"""
-    states = storage.all('State')
-    amenities = storage.all('Amenity')
-    return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
-@app.teardown_appcontext
-def teardown_db(*args, **kwargs):
-    """Close database or file storage"""
-    storage.close()
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
